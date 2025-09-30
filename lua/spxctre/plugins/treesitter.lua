4c84e2b086a92b83d17c161d7ee8a6065de64e1a
onlyspxctre <onlyspxctre@proton.me>
2025-09-30 00:05:45 -0400

    Make Treesitter `VeryLazy` to not block UI

--- lua/spxctre/plugins/treesitter.lua
+++ lua/spxctre/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = "UIEnter",
+    event = "VeryLazy", -- workaround to make highlighting non-blocking for UI
     branch = "master",
     build = ":TSUpdate",
     opts = {
