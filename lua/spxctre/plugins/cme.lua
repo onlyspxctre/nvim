e9f89ae8218ffa1b6ca1d768e7ac959f67079e45
onlyspxctre <onlyspxctre@proton.me>
2025-12-13 20:27:09 -0600

    Improved `cme` input handling
    Previously, a nasty error would occur when attempting to escape from the
    input dialog. This has been addressed.

--- lua/spxctre/plugins/cme.lua
+++ lua/spxctre/plugins/cme.lua
@@ -3,7 +3,19 @@ return {
     event = 'VeryLazy',
     config = function()
         vim.keymap.set('n', '<leader>mx', function()
-            vim.cmd('Compile ' .. vim.fn.input('Command: ', vim.g.cme_last_cmd or 'make -k'))
+            vim.ui.input({
+                prompt = 'Command: ',
+                default = vim.g.cme_last_cmd or 'make -k',
+            }, function(input)
+                if input == '' or input == nil then
+                    vim.defer_fn(function ()
+                        vim.cmd.echon('""')
+                    end, 1)
+                    return
+                end
+
+                vim.cmd('Compile ' .. input)
+            end)
         end)
     end
 }
