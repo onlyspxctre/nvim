026df24e093ff2b554a595c5346e60086115bbcc
onlyspxctre <onlyspxctre@proton.me>
2025-12-14 16:12:14 -0600

    `lazy.nvim` do not use `hererocks`, bootstrap with `--depth 1`
    We do not need luarocks support nor do we need to clone the entire
    repository commit history for lazy.nvim, as we intend to use it only.

--- lua/spxctre/lazy.lua
+++ lua/spxctre/lazy.lua
@@ -1,51 +1,54 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--depth 1', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out,                            'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require('lazy').setup({
-	performance = {
-		rtp = {
-			disabled_plugins = {
+    performance = {
+        rtp = {
+            disabled_plugins = {
                 'gzip',
                 'tarPlugin',
                 'zipPlugin',
                 -- 'matchit',
                 'matchparen',
-				'tutor',
+                'tutor',
                 'tohtml',
                 -- unstable --
                 'shada',
                 'rplugin',
                 'editorconfig',
                 'spellfile',
-			},
-		},
-	},
-  spec = {
-    { import = 'spxctre.plugins' },
-  },
-  defaults = { lazy = true },
-  ui = {
-	  title = 'Lazy.nvim',
-	  backdrop = 100,
-	  border = 'rounded',
-  },
+            },
+        },
+    },
+    rocks = {
+        enabled = false,
+    },
+    spec = {
+        { import = 'spxctre.plugins' },
+    },
+    defaults = { lazy = true },
+    ui = {
+        title = 'Lazy.nvim',
+        backdrop = 100,
+        border = 'rounded',
+    },
 
-  -- Make lazy.nvim not annoying as fuck
-  checker = { enabled = false },
-  change_detection = { notify  = false },
+    -- Make lazy.nvim not annoying as fuck
+    checker = { enabled = false },
+    change_detection = { notify = false },
 })
