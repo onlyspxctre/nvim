e62beb126f10d09819b97cb3517b64034be535c1
onlyspxctre <onlyspxctre@proton.me>
2025-12-29 13:41:54 -0600

    cme: auto-focus window
    Added an autocmd rule to auto-focus the window when it is created.

--- init.lua
+++ init.lua
@@ -13,9 +13,27 @@ vim.api.nvim_create_autocmd('FileType', {
     end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'qf' },
+    callback = function(ev)
+        vim.defer_fn(function()
+            vim.api.nvim_set_current_win(vim.fn.win_findbuf(ev.buf)[1])
+        end, 5) -- deferred to make sure autocmd catches the window init after init has finished
+    end
+})
+
 vim.api.nvim_create_autocmd({ 'ColorScheme', 'UIEnter' }, {
     callback = function()
         vim.api.nvim_set_hl(0, 'Pmenu', {
+            link = 'Normal'
+        })
+        vim.api.nvim_set_hl(0, 'NormalFloat', {
+            link = 'Normal'
+        })
+        vim.api.nvim_set_hl(0, 'FloatBorder', {
+            link = 'Normal'
+        })
+        vim.api.nvim_set_hl(0, 'TelescopeBorder', {
             link = 'Normal'
         })
     end
