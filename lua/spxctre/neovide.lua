57b6460f389464c726208c3cfe116b0bd322771c
onlyspxctre <onlyspxctre@proton.me>
2026-06-13 10:20:41 -0400

    neovide: random animation changes
    I don't know where these are from but I guess they make Neovide
    slightlhy better idk

--- lua/spxctre/neovide.lua
+++ lua/spxctre/neovide.lua
@@ -1,7 +1,8 @@
 if vim.g.neovide then
     vim.o.guifont = 'IosevkaTerm Nerd Font Mono:h21'
 
-    vim.g.neovide_cursor_animation_length = 0.05
-    vim.g.neovide_scroll_animation_length = 0.10
+    vim.g.neovide_cursor_animation_length = 0.01
+    vim.g.neovide_cursor_short_animation_length = 0.02
+    vim.g.neovide_scroll_animation_length = 0.02
     vim.g.neovide_cursor_trail_size = 0.2
 end
