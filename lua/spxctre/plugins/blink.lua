732a9e36ab4ff6df9bef753abadd7bc53a1bbc42
onlyspxctre <onlyspxctre@proton.me>
2026-07-13 23:58:23 -0400

    blink: disable autocomplete by default
    Autocomplete must now be manually requested by using the <C-Space>
    shortcut to pull up the menu. This is to assist with distraction-free
    programming and an overall lesser reliance on autocomplete.

--- lua/spxctre/plugins/blink.lua
+++ lua/spxctre/plugins/blink.lua
@@ -17,7 +17,7 @@ return {
             menu = {
                 enabled = true,
                 border = 'rounded',
-                auto_show = true,
+                auto_show = false,
             },
             documentation = {
                 auto_show = false,
